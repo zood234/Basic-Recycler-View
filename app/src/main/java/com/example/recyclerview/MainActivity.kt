367c6c4835ce35e548d5a883d6920f47c8e8a331
zood234 <zainmahmood@live.co.uk>
2021-07-15 14:40:47 +0100

    Basic Recycler View takes an array and then puts it in the view

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,36 +1,78 @@
 package com.example.recyclerview
 
-
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsappwithapi.NewsApi
+import com.example.newsappwithapi.dataWeb.NewsResponse
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView()
 
-        // Set the LayoutManager that this RecyclerView will use.
-        recycler_view_items.layoutManager = LinearLayoutManager(this)
+    }
 
+fun getApiData(): ArrayList<String>{
+    var list = ArrayList<String>()
+
+    //This one gets added
+    list.add("THIS STUFF")
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.nytimes.com/svc/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val service = retrofit.create(NewsApi::class.java)
+    val call = service.getNews()
+    call.enqueue(object : Callback<NewsResponse> {
+
+        //I Thinks this part is the problem  for some for some reason recyclerView() just skips it
+        override fun onResponse(call: Call<NewsResponse>, response: Response<NewsResponse>, ) {
+
+
+            if (response.code() == 200) {
+                val newsResponse = response.body()!!
+
+                list.add("This WAS NOT ADDED")
+                list.add(newsResponse.results[2].title)
+                list.add(newsResponse.results[3].title)
+
+                val stringBuilder = "Section is : " + newsResponse.section +
+                        "\n" + list[1] +
+                        "\n" + newsResponse.results[1].title +
+                        "\n" + newsResponse.results[2].title +
+                        "\n" + newsResponse.results[3].title +
+                        "\n" + newsResponse.results[4].title + "bjhkh ddgg"
+                textView.text = stringBuilder
+            }
+        }
+        override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+        }
+    })
+    return list
+
+}
+
+
+    fun recyclerView(){
         // Adapter class is initialized and list is passed in the param.
-        val itemAdapter = ItemAdapter(this, getItemsList())
+        val itemAdapter = ItemAdapter(this, getApiData())
+
+        recycler_view_items.layoutManager = LinearLayoutManager(this)
 
         // adapter instance is set to the recyclerview to inflate the items.
         recycler_view_items.adapter = itemAdapter
     }
 
-    /**
-     * Function is used to get the Items List which is added in the list.
-     */
-    private fun getItemsList(): ArrayList<String> {
-        val list = ArrayList<String>()
 
-        for (i in 1..15) {
-            list.add("Item $i")
-        }
 
-        return list
-    }
 }
--- app/src/main/java/com/example/recyclerview/itemAdapter.kt
+++ app/src/main/java/com/example/recyclerview/itemAdapter.kt
@@ -12,12 +12,6 @@ import kotlinx.android.synthetic.main.item_custom_row.view.*
 class ItemAdapter(val context: Context, val items: ArrayList<String>) :
     RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
-    /**
-     * Inflates the item views which is designed in xml layout file
-     *
-     * create a new
-     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(context).inflate(
@@ -28,55 +22,20 @@ class ItemAdapter(val context: Context, val items: ArrayList<String>) :
         )
     }
 
-    /**
-     * Binds each item in the ArrayList to a view
-     *
-     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
-     * an item.
-     *
-     * This new ViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
 
         val item = items.get(position)
-
-        //put my hyperlink here
-        holder.cardViewItem.setOnClickListener{
-        }
-
+        holder.cardViewItem.setOnClickListener{}
         holder.tvItem.text = item
-        // Updating the background color according to the odd/even positions in list.
-        if (position % 2 == 0) {
-//            holder.cardViewItem.setBackgroundColor(
-//                ContextCompat.getColor(
-//                    context,
-//                    R.color.colorLightGray
-//                )
-//            )
-        }
-//         else {
-//            holder.cardViewItem.setBackgroundColor(
-//                ContextCompat.getColor(
-//                    context,
-//                    R.color.colorWhite
-//                )
-//            )
-//        }
     }
 
-    /**
-     * Gets the number of items in the list
-     */
+
     override fun getItemCount(): Int {
         return items.size
     }
 
-    /**
-     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
-     */
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Holds the TextView that will add each item to
         val tvItem = view.tv_item_name
